041925edac58773b0aeb2e2f3151a953048622ff
javi2gar <javi2gar@gmail.com>
2024-05-31 01:27:49 +0100

    US0001 - Created initial config (spring initializr)

---
+++ src/main/resources/db/migration/h2/V3.0__create_fees_table.sql
@@ -0,0 +1,9 @@
+create table FEES
+(
+    FEE_ID BIGINT           default ' '     not null,
+    NAME    CHAR(35)        default ' '     not null,
+    AMOUNT  NUMERIC(20,2)   default 0       not null,
+
+    constraint PK_FEE
+        primary key (FEE_ID)
+);
