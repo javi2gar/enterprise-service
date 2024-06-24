915700dad5fbf369d9f9a852ef83739a77195427
javi2gar <javi2gar@gmail.com>
2024-06-24 09:53:49 +0100

    Fix CORS

--- src/main/java/com/enterprise/service/infrastructure/config/CORSConfig.java
+++ src/main/java/com/enterprise/service/infrastructure/config/CORSConfig.java
@@ -11,7 +11,7 @@ public class CORSConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET")
-                .allowedOrigins("*")
+                .allowedOrigins("enterprise-service.com")
                 .allowedHeaders("*");
     }
 
